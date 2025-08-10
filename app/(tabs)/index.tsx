62ee01278d6d4a32564416c621ceef67ad77d8ce
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-10 11:55:40 +0300

    Set up the folder stracture

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="w-full h-screen flex items-center justify-center">
+      <Text className="text-4xl font-semibold text-orange-400">
+        This is Natnael.
+      </Text>
+    </View>
+  );
+}
