1dcc26bfada4f6403812dfc8fe88bbe89274e381
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-11 16:55:09 +0300

    Add router props

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,7 +2,11 @@ import { icons } from "@/constants/icons";
 import React from "react";
 import { Image, Text, TextInput, View } from "react-native";
 
-const SearchBar = () => {
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+const SearchBar = ({ placeholder, onPress }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
