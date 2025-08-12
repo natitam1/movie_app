8fb78ba95d5de8c27812f0f6c64013e12aead6d6
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-08-12 08:31:44 +0300

    Connect api with the sorter

--- services/api.ts
+++ services/api.ts
@@ -1,14 +1,40 @@
-const url = "https://api.themoviedb.org/3/authentication";
-const options = {
-  method: "GET",
+export const TMDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
   headers: {
     accept: "application/json",
-    Authorization:
-      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTNlYjdhOTQ3MWNlZWU4OTI4NTgxMWU5MWM2YjBhNSIsIm5iZiI6MTc0NDM3MjM3OC4xLCJzdWIiOiI2N2Y5MDI5YWVhODBkODUxNzU5OWE0Y2EiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.aLE3NaT_6eFJi85WrBdMRp9JGjIAJ3CBklOE6o83CGU",
+    authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
   },
 };
 
-fetch(url, options)
-  .then((res) => res.json())
-  .then((json) => console.log(json))
-  .catch((err) => console.error(err));
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+  const data = await response.json();
+  return data.results;
+};
+
+// const url = "https://api.themoviedb.org/3/authentication";
+// const options = {
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYTNlYjdhOTQ3MWNlZWU4OTI4NTgxMWU5MWM2YjBhNSIsIm5iZiI6MTc0NDM3MjM3OC4xLCJzdWIiOiI2N2Y5MDI5YWVhODBkODUxNzU5OWE0Y2EiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.aLE3NaT_6eFJi85WrBdMRp9JGjIAJ3CBklOE6o83CGU",
+//   },
+// };
+
+// fetch(url, options)
+//   .then((res) => res.json())
+//   .then((json) => console.log(json))
+//   .catch((err) => console.error(err));
